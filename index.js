ec3a65189a4cc592ee8f9952ed95f891dcc94bf3
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 20:15:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var katzDeliLine = [];
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-for (let i = 1; i< katzDeliLine.length; i++){
     //adds the person's name to the deli line array
-    return `Welcome, ${name(i)}. You are number ${katzDeliLine.length} in line.`
+    return "Welcome," + name + ". You are number" + katzDeliLine.length +" in line."
   }
 }
 //returns the person's name with their place in line
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!"
+  } else {
+    var name = katzDeliLine.shift(0);
+  }
+  function currentLine(katzDeliLine) {
+      if (katzDeliLine.length === 0) {
+        return "The line is currently empty."
+      } else {
+        return `The Line is currently : ${katzDeliLine(i)} + . ${name(i)}`;
+        }
+    }
