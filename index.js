47e4d048171a9e7601273f078edcbc668ba03758
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 20:25:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!"
   } else {
-    var firstPerson = []''
+    var firstPerson = [];
     firstPerson = katzDeliLine.shift(0);
     return "Currently serving " + firstPerson;
   }
