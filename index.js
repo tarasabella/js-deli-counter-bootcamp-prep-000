a202b0d61e50690287a0d30578a33b4600ce84d2
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 20:12:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,16 +7,3 @@ for (let i = 1; i< katzDeliLine.length; i++){
   }
 }
 //returns the person's name with their place in line
-function nowServing(katzDeliLine) {
-  if (katzDeliLine.length === 0) {
-    return "There is nobody waiting to be served!"
-  } else {
-    var name = katzDeliLine.shift(0);
-  }
-  function currentLine(katzDeliLine) {
-      if (katzDeliLine.length === 0) {
-        return "The line is currently empty."
-      } else {
-        return `The Line is currently : ${katzDeliLine(i)} + . ${name(i)}`;
-        }
-    }
