3ab21f86d1c205b09226227ceaa8ec10eb2e4c7d
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 20:40:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,5 +17,5 @@ function nowServing(katzDeliLine) {
 function currentLine(katzDeliLine) {
   for (let i = 1; i < katzDeliLine.length; i++) {
     if (katzDeliLine.length === 0) {
-      return "The line is currently empty."
-} else {
+      return "The line is currently empty." }
+    }
