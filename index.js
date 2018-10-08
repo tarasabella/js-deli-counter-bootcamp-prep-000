cc0a23de95ec726806593c78c4e5790ab93ab4ba
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 19:38:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+function takeANumber(line, name) {
+for (let n = 1; n < line.length; i++) {
+  return line.push(`Welcome, ${name}. You are number ${line.length} in line.`);
+
+
+function nowServing(line) {
+  if (!line.length) {
+    return "There is nobody waiting to be served!";
+  } else {
+  return `Currently serving ${line.shift()}.`
+}
+}
+
+function currentLine(line) {
+  if (!line.length) {
+    return "The line is currently empty."
+  }
+
+  const numbersAndNames = [];
+
+  for (let i = 0, l = line.length; i < l; i++) {
+    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+  }
+
+  return `The line is currently: ${numbersAndNames.join(', ')}`;
+}
