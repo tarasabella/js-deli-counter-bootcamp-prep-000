3c18cb415b02fe1b12221988f93c591f4f874bc9
Tara E. Sabella <tarasabella@gmail.com>
2018-10-08 20:13:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
 for (let i = 1; i< katzDeliLine.length; i++){
     //adds the person's name to the deli line array
-    return "Welcome," + name + ". You are number katzDeliLine.length in line."
+    return `Welcome, ${name(i)}. You are number ${katzDeliLine.length} in line.`
   }
 }
 //returns the person's name with their place in line
